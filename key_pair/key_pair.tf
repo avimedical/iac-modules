8efc93b60bedef0e681b3e0c16dd337b88804d46
Ashutosh Angiras <ashu.angiras11@gmail.com>
2023-09-11 11:32:59 +0200

    update module

--- key_pair/key_pair.tf
+++ key_pair/key_pair.tf
@@ -9,3 +9,7 @@ output "pub_key" {
 output "key_pair_name" {
   value = opentelekomcloud_compute_keypair_v2.key_pair.name
 }
+
+output "priv_key" {
+  value = opentelekomcloud_compute_keypair_v2.key_pair.private_key
+}
