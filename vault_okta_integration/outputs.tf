02aa6c6c2abd273a85787e6afbd55ca9f92be047
Ashutosh Angiras <ashu.angiras11@gmail.com>
2023-09-18 06:19:28 +0200

    add vault okta integration module

---
+++ vault_okta_integration/outputs.tf
@@ -0,0 +1,15 @@
+
+
+output "roles" {
+  description = "Role names created by this module"
+  value       = keys(vault_jwt_auth_backend_role.okta_role)
+}
+
+output "path" {
+  description = "Okta OIDC auth path"
+  value       = vault_jwt_auth_backend.okta_oidc.path
+}
+
+output "accessor" {
+  value       = vault_jwt_auth_backend.okta_oidc.accessor
+}
