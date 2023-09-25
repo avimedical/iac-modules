8e3ee8ba46c55bfbad3443292222f95f3b652938
Ashutosh Angiras <ashu.angiras11@gmail.com>
2023-09-25 03:03:28 +0200

    udpate lb dependencies

--- ecs/loadbalancer.tf
+++ ecs/loadbalancer.tf
@@ -8,31 +8,38 @@ resource "opentelekomcloud_lb_loadbalancer_v2" "ecs_loadbalancer" {
 }
 
 resource "opentelekomcloud_lb_listener_v2" "ecs_loadbalancer_listener" {
+  
+  count         = var.loadbalancer_setup.enable == true ? 1 : 0
+
   name                      = "${var.instance_name}-listener"
   loadbalancer_id           = opentelekomcloud_lb_loadbalancer_v2.ecs_loadbalancer[0].id
   protocol                  = var.loadbalancer_setup.listener_protocol
   protocol_port             = var.loadbalancer_setup.listener_port
   default_tls_container_ref = var.loadbalancer_setup.listener_tls_id
 
-  depends_on = [ opentelekomcloud_lb_loadbalancer_v2.ecs_loadbalancer ]
+  depends_on = [ opentelekomcloud_lb_loadbalancer_v2.ecs_loadbalancer[0] ]
 }
 
 resource "opentelekomcloud_lb_pool_v2" "ecs_loadbalancer_pool" {
+    count         = var.loadbalancer_setup.enable == true ? 1 : 0
+
   name        = "${var.instance_name}-pool"
   protocol    = var.loadbalancer_setup.pool_protocol
-  listener_id = opentelekomcloud_lb_listener_v2.ecs_loadbalancer_listener.id
+  listener_id = opentelekomcloud_lb_listener_v2.ecs_loadbalancer_listener[0].id
   lb_method   = var.loadbalancer_setup.pool_method
 
-  depends_on = [ opentelekomcloud_lb_listener_v2.ecs_loadbalancer_listener ]
+  depends_on = [ opentelekomcloud_lb_listener_v2.ecs_loadbalancer_listener[0] ]
 }
 
 resource "opentelekomcloud_lb_member_v2" "ecs_loadbalancer_pool_members" {
+    count         = var.loadbalancer_setup.enable == true ? 1 : 0
+
   address       = opentelekomcloud_compute_instance_v2.this.access_ip_v4
   protocol_port = 8200
-  pool_id       = opentelekomcloud_lb_pool_v2.ecs_loadbalancer_pool.id
+  pool_id       = opentelekomcloud_lb_pool_v2.ecs_loadbalancer_pool[0].id
   subnet_id     = var.subnet_id
 
-  depends_on = [ opentelekomcloud_lb_pool_v2.ecs_loadbalancer_pool ]
+  depends_on = [ opentelekomcloud_lb_pool_v2.ecs_loadbalancer_pool[0] ]
 }
 
 resource "opentelekomcloud_dns_recordset_v2" "ecs_dns_private" {
@@ -44,5 +51,5 @@ resource "opentelekomcloud_dns_recordset_v2" "ecs_dns_private" {
   type        = "A"
   records     = var.loadbalancer_setup.enable == true ? [opentelekomcloud_lb_loadbalancer_v2.ecs_loadbalancer[0].vip_address] : [opentelekomcloud_compute_instance_v2.this.access_ip_v4]
 
-  depends_on = [ opentelekomcloud_lb_listener_v2.ecs_loadbalancer_listener ]
+  depends_on = [ opentelekomcloud_lb_listener_v2.ecs_loadbalancer_listener[0] ]
 }
