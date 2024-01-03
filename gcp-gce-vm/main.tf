215dc36ba1b8769dc63715ecee304f05eb3cf95c
Ashutosh Angiras <ashu.angiras11@gmail.com>
2024-01-03 16:23:51 +0100

    update mod

--- gcp-gce-vm/main.tf
+++ gcp-gce-vm/main.tf
@@ -1,9 +1,4 @@
 
-provider "google" {
-  project = var.project_id
-  region  = var.region
-}
-
 module "instance_template" {
   source  = "terraform-google-modules/vm/google//modules/instance_template"
   version = "~> 10.0"
