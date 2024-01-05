b6afd3959590178209fa3a1578d457893b923ec8
Ashutosh Angiras <ashu.angiras11@gmail.com>
2024-01-05 13:17:14 +0100

    update lb module

--- gcp-gce-lb/lb.tf
+++ gcp-gce-lb/lb.tf
@@ -24,13 +24,13 @@ resource "google_compute_url_map" "url_map" {
 
 resource "google_compute_backend_service" "backend_service" {
   provider = google-beta
-  
+
   name                            = "${var.name}-backend-service"
   enable_cdn                      = true
   timeout_sec                     = 10
   connection_draining_timeout_sec = 10
 
-  custom_request_headers          = ["host: ${google_compute_global_network_endpoint.proxy.fqdn}"]
+  custom_request_headers          = ["host: ${google_compute_global_network_endpoint.endpoint.fqdn}"]
   custom_response_headers         = ["X-Cache-Hit: {cdn_cache_status}"]
 
   backend {
