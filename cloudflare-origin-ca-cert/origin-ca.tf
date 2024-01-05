8a44af120b3c11e9a2f3ded48f998c8f7be1ab3d
Ashutosh Angiras <ashu.angiras11@gmail.com>
2024-01-05 10:55:40 +0100

    add cloudflare cert module

---
+++ cloudflare-origin-ca-cert/origin-ca.tf
@@ -0,0 +1,25 @@
+resource "tls_private_key" "private_key" {
+  algorithm = "RSA"
+}
+
+resource "tls_cert_request" "csr" {
+  private_key_pem = tls_private_key.private_key.private_key_pem
+
+  subject {
+    common_name  = var.common_name
+    organization = "Avimedical GmbH"
+    organizational_unit = "Infrastructure"
+    street_address = "Ridlerstraße 37-39"
+    locality = "Munich"
+    state = "Bavaria"
+    country = "DE"
+    postal_code = "80339"
+  }
+}
+
+resource "cloudflare_origin_ca_certificate" "ca_cert" {
+  csr                = tls_cert_request.csr.cert_request_pem
+  hostnames          = var.hostnames
+  request_type       = "origin-rsa"
+  requested_validity = var.validity_days
+}
--- gcp-gce-lb/lb.tf
+++ gcp-gce-lb/lb.tf
@@ -25,3 +25,31 @@ resource "google_compute_http_health_check" "health_check" {
   timeout_sec        = 1
   port               = var.target_port
 }
+
+resource "google_compute_network_endpoint_group" "endpoint_group" {
+  name        = "${var.name}-endpoint-group"
+  network     = var.network
+  subnetworks = var.subnetworks
+  default_port = var.target_port
+  zone         = var.zone
+  network_endpoint_type = "GCE_VM_IP_PORT"
+}
+
+resource "google_compute_network_endpoint" "endpoint" {
+  network_endpoint_group = google_compute_network_endpoint_group.endpoint_group.name
+  port = var.target_port
+  ip_address = var.target_ip
+}
+
+resource "google_compute_backend_service" "backend_service" {
+  name        = "${var.name}-backend-service"
+  port_name   = "http"
+  protocol    = "HTTP"
+  timeout_sec = 30
+
+  backend {
+    group           = google_compute_network_endpoint_group.endpoint_group.id
+    balancing_mode  = "UTILIZATION"
+    max_utilization = 0.8
+  }
+}
