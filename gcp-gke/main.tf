4787ca898e4da899c92bf5afd29130341902d665
Ashutosh Angiras <ashu.angiras11@gmail.com>
2024-01-16 14:29:46 +0100

    update variables

--- gcp-gke/main.tf
+++ gcp-gke/main.tf
@@ -34,7 +34,7 @@ module "gke" {
   master_authorized_networks = [
     {
       cidr_block   = data.google_compute_subnetwork.subnetwork.ip_cidr_range
-      display_name = "${var.cluster_name}-VPC"
+      display_name = "${var.cluster_name}-vpc"
     },
   ]
 
