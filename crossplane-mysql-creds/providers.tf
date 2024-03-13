f17cc304cfd6b7747c3672090ae7450d4da8c959
Pedro Mendes <pedro.avillez.mendes@gmail.com>
2024-03-14 00:00:52 +0100

    INFRA-1596 Commented required_providers block

--- crossplane-mysql-creds/providers.tf
+++ crossplane-mysql-creds/providers.tf
@@ -1,8 +1,8 @@
-terraform {
-  required_version = ">=0.13.0"
-  required_providers {
-    kubernetes = {
-      source = "hashicorp/kubernetes"
-    }
-  }
-}
+# terraform {
+#   required_version = ">=0.13.0"
+#   required_providers {
+#     kubernetes = {
+#       source = "hashicorp/kubernetes"
+#     }
+#   }
+# }
