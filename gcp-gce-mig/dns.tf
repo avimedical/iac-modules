9e390998bbe27dcbb3e33fe6de913e0412ac004e
Miro Michalicka <miro@happtiq.com>
2024-04-12 07:41:30 +0200

    fix: Use variables instead of outputs.

--- gcp-gce-mig/dns.tf
+++ gcp-gce-mig/dns.tf
@@ -1,5 +1,5 @@
 locals {
-  hostnames = var.dns.create_dns_record && try(module.ilb[0].ip_address, null) != null ? { for idx, hostname in var.dns.create_dns_record : idx => hostname } : {}
+  hostnames = var.dns.create_dns_record && var.ilb_enabled == true ? var.dns.hostnames : []
 }
 
 data "google_dns_managed_zone" "zone" {
